757adf601a115acdd3e649a1bd005fb6871c1951
Đào An <daoan1412@gmail.com>
2020-07-01 18:34:51 +0700

    6:30

--- next.config.js
+++
@@ -1,6 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://raw.githubusercontent.com/daoan1412/cv/master/docs/' : '',
-}
